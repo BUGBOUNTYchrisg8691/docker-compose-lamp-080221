c2c3a6a38f0fc6b44043b1400ed90876999d228e
Chris Girvin <bugbounty.chrisg8691@gmail.com>
2021-08-03 01:57:33 -0400

    src was in gitignore for some reason; i believe for archlinuxpackages arg i passed to gi

---
+++ src/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$HOST = 'mysql';
+$USER = 'api-admin-dev';
+$PASS = 'password';
+$DB = 'api_dev';
+
+$conn = new mysqli($HOST, $USER, $PASS, $DB);
+
+if ($conn->connect_error) {
+  echo 'Connection failed ' . $conn->connect_error;
+} else {
+  echo 'Successfully connected to database: "' . $DB . '" via MySQL';
+}
+
+echo "<br>";
+
+$APACHE_VER = apache_get_version();
+if ($APACHE_VER) {
+  echo 'Apache version: "' . $APACHE_VER . '"';
+} else {
+  echo 'Failed to get Apache version.';
+}
+
+?>
